f7b5e61950242976a7f6aac2c63a1eb41bc3756c
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-06-02 00:34:45 +0300

    Tailwind CSS - Marvel

---
+++ dist/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                nav: false
+            },
+            768: {
+                nav: true
+            },
+        }
+    });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                "maven": ["Maven Pro", "sans-serif"]
+            },
+            colors: {
+                textColor: "#5b5b5b",
+                grey: "#EEEEEE",
+                secondryColor: "#47465A",
+                headingColor: "#353535",
+                headingColor2: "#484848"
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
+}
